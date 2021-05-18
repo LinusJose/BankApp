fcda88911b16cd64c6cd71727cf3034f1b3d8d9a
linus elias jose <linuseliasg6@gmail.com>
2021-05-18 11:40:55 +0530

    bankapp

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,51 @@
+
+
+
+
+import { Component, OnInit } from '@angular/core';
+import{Router} from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+aim="your perfect partner"
+acno="account number please"
+pswd="";
+ accountDetails:any = {
+  1000: { acno: 1000,  username: "userone", password: "userone", balance: 50000 },
+  1001: { acno: 1001, username: "usertwo", password: "usertwo", balance: 5000 },
+  1002: { acno: 1002,  username: "userthree", password: "userthree", balance: 10000 },
+  1003: { acno: 1003,  username: "userfour", password: "userfour", balance: 6000 }
+};
+  constructor(private router:Router) { }
+
+  ngOnInit(): void {
+  }
+
+  
+  login(){
+  var acno = this.acno;
+  var pswd=this.pswd;
+  let users=this.accountDetails;
+  if (acno in users) {
+
+    if(pswd == users[acno]["password"]){
+      alert("login success");
+      this.router.navigateByUrl("dashboard")
+    }
+  
+    else{
+     alert("incorrect password")//invalid username or password
+    }
+  }
+  else{
+    alert("invalid acount")//invalid account number
+  }
+}
+register(){
+  this.router.navigateByUrl("register");
+}
+}
